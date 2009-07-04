35795f72c4f655ee24952553fc1adb93f8dd0ce5
Nick Welch <nick@incise.org>
2009-07-03 19:16:53 -0700

    tweaks

--- xstartup.sh
+++ xstartup.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-aterm &
-
 cd /
 
 xsetroot -solid black -cursor_name left_ptr
 Esetroot -scale ~/images/blur4.png &
-rempd start >/dev/null &
 xset b off
 xset m
 xset s off
 xmodmap -e 'pointer = 1 2 3 6 7 4 5 8 9'
 xset r rate 210 83
-unclutter -idle 1.5 -jitter 1 &
-if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
-    eval `dbus-launch --sh-syntax --exit-with-session`
-fi
+
+#if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
+#    eval `dbus-launch --sh-syntax --exit-with-session`
+#fi
+
+sh ~/.metacity.conf
+metacity &
 
